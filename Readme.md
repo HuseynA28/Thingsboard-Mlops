7f18b28b430595a7471d8713b6f6ed741b92780c
Huseyn Abdullayev <abdulla.huseyn@yahoo.com>
2024-11-24 22:01:02 +0100

    Updated

--- Readme.md
+++ Readme.md
@@ -1,13 +1,1 @@
-First Create volumns 
 
-docker volume create mytrendz-data
-docker volume create mytrendz-data-db
-docker volume create mytrendz-logs
-
-docker volume create mytb-data
-docker volume create mytb-logs
-
-Do not forget to change 8080  Thingsbaor to Publich address
-
-
-https://thingsboard.io/pricing/?section=trendz-optionsTrendz
--- myFastapi/main.py
+++ myFastapi/main.py
@@ -1,10 +1,7 @@
 from typing import List, Dict, Any, Set, Tuple, Optional
 import httpx
 from fastapi import FastAPI, HTTPException, Path, Query, Depends, Body
-try: 
-    from myFastapi.snowflake_utils import *
-except:
-    from snowflake_utils import *
+
 try:
     from myFastapi.funtions import convert_time, fetch_all_telemetry
 except:
@@ -123,43 +120,3 @@ async def get_elements_by_id(
 
 
 
-# @app.get(
-#     "/elements/{entityType}/{entityId}",
-#     responses={
-#         400: {"description": "Invalid parameter supplied"},
-#         401: {"description": "Unauthorized. Please login."},
-#         404: {"description": "No element(s) found"},
-#     },
-#     tags=["Elements"],
-#     summary="Get all element_uid's between start and end date",
-# )
-
-# async def get_elements_route(
-#     entityType: str = Path(..., description="String value representing the entity type. For example, 'DEVICE'"),
-#     entityId: str = Path(..., description="A string value representing the entity id. For example, '784f394c-42b6-435a-983c-b7beff2784f9'"),
-#     start_date: str = Query(..., description="Start time/date in UTC format, e.g., '2023-04-23T17:25:43.511Z'", alias="start-date"),
-#     end_date: str = Query(..., description="End time/date in UTC format, e.g., '2023-04-23T17:25:43.511Z'", alias="end-date"),
-#     telemetry_keys: Optional[str] = Query(None, description="Comma-separated list of telemetry keys.")
-# ):
-#     global token_global 
-#     if not token_global:
-#         raise HTTPException(status_code=401, detail="User not authenticated. Please login. There is no token_global.")
-
-#     # Convert start_date and end_date to milliseconds
-#     start_time_millis, end_time_millis = convert_time(start_date, end_date)
-
-#     # Convert telemetry_keys to a list
-#     telemetry_keys_list = telemetry_keys.split(",") if telemetry_keys else None
-
-#     async with httpx.AsyncClient() as client:
-#         telemetry = await fetch_all_telemetry(
-#             entityType=entityType,
-#             client=client,
-#             entityId=entityId,
-#             start_time_millis=start_time_millis,
-#             end_time_millis=end_time_millis,
-#             telemetry_keys=telemetry_keys_list,
-#             token=token_global
-#         )
-     
-#     return telemetry 
